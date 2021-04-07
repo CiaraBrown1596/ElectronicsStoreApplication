6eb8dad08a7cc159c9ff1746f517a6344302fbe5
CiaraBrown1596 <ciarabrown1596@gmail.com>
2021-04-07 18:42:27 +0100

    Welcome page Login/sign up buttons

--- app/src/main/java/com/example/electronicsstoreapplication/MainActivity.java
+++ app/src/main/java/com/example/electronicsstoreapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.electronicsstoreapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button LoginBtn, SignUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoginBtn = findViewById(R.id.buttonLogin);
+        LoginBtn = findViewById(R.id.buttonLogin);
     }
 }
